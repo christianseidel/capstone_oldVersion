9dc2e1f0747a7f03bac6bfaa17a72e78fd111c3f
Christian Seidel <christian@parameter5.de>
2022-04-17 11:37:58 +0200

    minor change

--- backend/src/main/java/de/neuefische/smartcount/SmartCountController.java
+++ backend/src/main/java/de/neuefische/smartcount/SmartCountController.java
@@ -4,7 +4,6 @@ import de.neuefische.smartcount.Exceptions.InvalidUserException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
