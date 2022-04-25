22f61d2e08c67902a5a004c3561c44a521bf0d7d
Christian Seidel <christian@parameter5.de>
2022-04-25 22:17:10 +0200

    Integration Test halfway through

--- backend/src/test/java/de/neuefische/smartcount/SmartCountControllerTest.java
+++ backend/src/test/java/de/neuefische/smartcount/SmartCountControllerTest.java
@@ -13,7 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
+
+import java.security.Principal;
 
 import static de.neuefische.smartcount.Currency.EUR;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,7 +36,7 @@ class SmartCountControllerTest {
 
         // should register new user
         UserCreationData createUser1 = new UserCreationData();
-        createUser1.setUsername("Alfred");
+        createUser1.setUsername("Jakob");
         createUser1.setPassword("234SoSafe");
         createUser1.setPasswordAgain("234SoSafe");
 
@@ -41,14 +47,12 @@ class SmartCountControllerTest {
 
         // should log in user
         UserLoginData loginUser1 = new UserLoginData();
-        loginUser1.setUsername("Alfred");
+        loginUser1.setUsername("Jakob");
         loginUser1.setPassword("234SoSafe");
 
         ResponseEntity<Token> responseLoginUser1 = restTemplate.postForEntity("/api/login", loginUser1, Token.class);
 
         assertThat(responseLoginUser1.getStatusCode()).isEqualTo(HttpStatus.OK);
-
-        // should log out user
 
         // should not log in user
         UserLoginData loginUser1f = new UserLoginData();
@@ -68,17 +72,17 @@ class SmartCountControllerTest {
 
         assertThat(responseLoginUser1twice.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
 
-        // log in user again
+        // (log in user again)
         UserLoginData loginNewUser1 = new UserLoginData();
-        loginNewUser1.setUsername("Alfred");
+        loginNewUser1.setUsername("Jakob");
         loginNewUser1.setPassword("234SoSafe");
         ResponseEntity<Token> loginResponse = restTemplate.postForEntity("/api/login", loginNewUser1, Token.class);
 
-        String token = loginResponse.getBody().toString();
+        String token = loginResponse.getBody().getToken();
         HttpHeaders bearerHeader = new HttpHeaders();
         bearerHeader.setBearerAuth(token);
 
-        // should add new expenses
+        // should add new expense
         Expense expense01 = new Expense();
         expense01.setPurpose("Grillkohle");
         expense01.setDescription("Aral");
@@ -93,35 +97,137 @@ class SmartCountControllerTest {
         );
 
         Expense createdExpense = createExpense01.getBody();
+        expense01.setId(createExpense01.getBody().getId());
+        expense01.setUser(createExpense01.getBody().getUser());
 
- //       Assertions.assertThat(createExpense01.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        Assertions.assertThat(createdExpense).isEqualTo(createExpense01);
+        Assertions.assertThat(createExpense01.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(createdExpense).isEqualTo(expense01);
 
-//        ResponseEntity<Expense> createExpense01 = restTemplate.postForEntity("/api/expenses", expense01, Expense.class);
-//        Assertions.assertThat(createExpense01.getStatusCode()).isEqualTo(HttpStatus.OK);
-
+        // create two additional expenses
         Expense expense02 = new Expense();
         expense02.setPurpose("Servietten");
         expense02.setDescription("Lidl");
         expense02.setAmount(2.75);
         expense02.setCurrency(EUR);
-        expense02.setUser("Andrea");
+        Expense expense03 = new Expense();
+        expense03.setPurpose("Gurken");
+        expense03.setDescription("Lidl");
+        expense03.setAmount(3.50);
+        expense03.setCurrency(EUR);
 
-    }
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense02, bearerHeader),Expense.class);
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense03, bearerHeader),Expense.class);
+
+        // sum should add up to 19.15 EUR
+        ResponseEntity<ExpensesDTO> expensesByJakob = restTemplate.exchange(
+                "/api/expenses/user",
+                HttpMethod.GET,
+                new HttpEntity<>(bearerHeader),
+                ExpensesDTO.class
+        );
+        double sum = expensesByJakob.getBody().getSum();
+        Assertions.assertThat(sum).isEqualTo(19.15);
+
+        // register second user
+        UserCreationData createUser2 = new UserCreationData();
+        createUser2.setUsername("Eva");
+        createUser2.setPassword("VerySafeToo");
+        createUser2.setPasswordAgain("VerySafeToo");
+        
+        ResponseEntity<String> responseRegisterUser2 = restTemplate.postForEntity("/api/register", createUser2, String.class);
+
+        assertThat(responseRegisterUser2.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        
+        // log in second user
+        UserLoginData loginUser2 = new UserLoginData();
+        loginUser2.setUsername("Eva");
+        loginUser2.setPassword("VerySafeToo");
+        ResponseEntity<Token> responseLoginUser2 = restTemplate.postForEntity("/api/login", loginUser2, Token.class);
+        assertThat(responseLoginUser2.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // get token
+        String tokenUser2 = responseLoginUser2.getBody().getToken();
+        HttpHeaders bearerHeaderUser2 = new HttpHeaders();
+        bearerHeaderUser2.setBearerAuth(tokenUser2);
+
+        // (create additional expenses)
+        Expense expense04 = new Expense();
+        expense04.setPurpose("Würstchen");
+        expense04.setDescription("Markt");
+        expense04.setAmount(15.60);
+        expense04.setCurrency(EUR);
+        Expense expense05 = new Expense();
+        expense05.setPurpose("Bauernbrot");
+        expense05.setDescription("Wochenmarkt");
+        expense05.setAmount(4.40);
+        expense05.setCurrency(EUR);
+        Expense expense06 = new Expense();
+        expense06.setPurpose("Senf");
+        expense06.setDescription("Rewe");
+        expense06.setAmount(3.75);
+        expense06.setCurrency(EUR);
+        Expense expense07 = new Expense();
+        expense07.setPurpose("2 Kästen Bier");
+        expense07.setDescription("Rewe");
+        expense07.setAmount(28.60);
+        expense07.setCurrency(EUR);
+        
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense04, bearerHeaderUser2),Expense.class);
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense05, bearerHeaderUser2),Expense.class);
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense06, bearerHeaderUser2),Expense.class);
+        restTemplate.exchange("/api/expenses",HttpMethod.POST,new HttpEntity<>(expense07, bearerHeaderUser2),Expense.class);
+
+        // check sum adds up to 52.35 EUR
+        ResponseEntity<ExpensesDTO> expensesByEva = restTemplate.exchange(
+                "/api/expenses/user",
+                HttpMethod.GET,
+                new HttpEntity<>(bearerHeaderUser2),
+                ExpensesDTO.class
+        );
+        double sumEva = expensesByEva.getBody().getSum();
+        Assertions.assertThat(sumEva).isEqualTo(52.35);
+
+        // create yet another expense and retrieve id
+        Expense expense08 = new Expense();
+        expense08.setPurpose("Salat");
+        expense08.setDescription("Rewe");
+        expense08.setAmount(2.60);
+        expense08.setCurrency(EUR);
+
+        ResponseEntity<Expense> createExpense08 = restTemplate.exchange(
+                "/api/expenses",
+                HttpMethod.POST,
+                new HttpEntity<>(expense08, bearerHeaderUser2),
+                Expense.class
+        );
+
+
+        // should change amount
+        String expense08Id = createExpense08.getBody().getId();
+        expense08.setId(expense08Id);
+        expense08.setUser("Eva");
+        expense08.setAmount(12.60);
+        System.out.println(expense08);
+      /*
+        ResponseEntity<Expense> changeAmountExpense08 = restTemplate.exchange(
+                "/api/expenses/" + expense08Id,
+                HttpMethod.PUT,
+                new HttpEntity<>(expense08Id, expense08, bearerHeaderUser2),
+                Expense.class
+        );
+        System.out.println(changeAmountExpense08);
+
+       */
+  //      Assertions.assertThat(changeAmountExpense08.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        Assertions.assertThat(changeAmountExpense08.getBody().getAmount()).isEqualTo(12.6);
+
 
 /*
-    @Test
-    void splittingUpExpenses() {
-
-       // ExpensesRepository testRepo = new ExpensesRepository();
-        SmartCountService testService = new SmartCountService(testRepo);
-
-        // verify if database is empty
-
-        // when
-        double sum = testService.getSum();
-        // then
-        Assertions.assertEquals(0, sum);
-    }
+        @PutMapping("/{id}")
+        public ResponseEntity<Expense> editExpense(@PathVariable String id, @RequestBody Expense expense, Principal principal) {
+            expense.setUser(principal.getName());
+            return ResponseEntity.of(smartCountService.editExpense(id, expense, principal.getName()));
+        }
 */
+    }
 }
