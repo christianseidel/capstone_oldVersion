dd1d9b960a9ad780cf7578a4812a5d9a785db06c
Christian Seidel <christian@parameter5.de>
2022-04-25 13:39:31 +0200

    balancing remodelled

--- backend/src/main/java/de/neuefische/smartcount/SmartCountService.java
+++ backend/src/main/java/de/neuefische/smartcount/SmartCountService.java
@@ -6,6 +6,7 @@ import de.neuefische.smartcount.Users.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 @Service
@@ -42,7 +43,7 @@ public class SmartCountService {
     public void deleteExpense(String id, String user) {
         var item = expensesRepository.findByIdAndUser(id, user);
         if (item.isEmpty()) {
-            throw new RuntimeException("Die Ausgabe mit der Id " + id + " ist nicht bekannt!");  // maybe rethink runtime exception !!??
+            throw new RuntimeException("An expense with Id no. " + id + " could not be found.");  // maybe rethink runtime exception !!??
         } else {
             expensesRepository.deleteById(id);
         }
@@ -66,85 +67,89 @@ public class SmartCountService {
                 .map(user -> user.getUsername()).toList();
     }
 
-    //  ----------------   Doing The Math   ----------------   //
+
     //  -----   Who needs to pay how much at the end   -----   //
 
     public List<TransactionsDTO> amountPerPerson() {
 
         List<User> userList = userRepo.findAll();
-        int numberOfPersons = userList.size();
-
-        // arithmetic mean of all expenses
-        double arithMean = Math.round(getSum()/numberOfPersons*100)/100.0;
-        if (arithMean==0.0) {
-            throw new RuntimeException("There is no sum big enough to be divided.");
-        }
+        double arithMean = getArithMean(userList);
 
         List<UserDOO> usersWithExcess = new ArrayList<>();
         List<UserDOO> usersWithDeficit = new ArrayList<>();
-
-        for (int i = 0; i < numberOfPersons; i++) {
-            String user = userList.get(i).getUsername();
-            double sum = getSumByUser(user);
-            if ((sum - arithMean) >= 0.0) {
-                double delta = Math.round((sum - arithMean)*100)/100.0;
-                var userRecord = new UserDOO (user, delta);
-                usersWithExcess.add(userRecord);
-            } else {
-                double delta = Math.round((arithMean - sum)*100)/100.0;
-                var userRecord = new UserDOO(user, delta);
-                usersWithDeficit.add(userRecord);
-            }
-        }
-
-        usersWithExcess.sort(Comparator.comparing(UserDOO::userDelta).reversed());
-        usersWithDeficit.sort(Comparator.comparing(UserDOO::userDelta).reversed());
-
-        int e = usersWithExcess.size()-1;
-        int d = usersWithDeficit.size()-1;
+        determinUserLists(userList, arithMean, usersWithExcess, usersWithDeficit);
 
         if (usersWithDeficit.size() == 0) {
             throw new RuntimeException("There is only one user in this group. No transaction will take place.");
         }
 
         // Make sure, all surpluses = all deficits
-        double sumPluses = 0.0;
-        for (int i = 0; i < usersWithExcess.size(); i++) {
-            sumPluses =  sumPluses + usersWithExcess.get(i).userDelta();
-        };
-        double sumMinuses = 0.0;
-        for (int i = 0; i < usersWithDeficit.size(); i++) {
-            sumMinuses =  sumMinuses + usersWithDeficit.get(i).userDelta();
-        };
+        double sumPluses = usersWithExcess.stream()
+                .mapToDouble(UserDOO::userDelta)
+                .sum();
+        double sumMinuses = usersWithDeficit.stream()
+                .mapToDouble(UserDOO::userDelta)
+                .sum();
+
         // Any difference will be added to the account of the group member with the biggest stock of expenses.
         // This ensures that the while loop will produce no glitch.
         double newDelta = Math.round((usersWithExcess.get(0).userDelta() - (sumPluses-sumMinuses))*100)/100.0;
         UserDOO firstUser = usersWithExcess.get(0);
         usersWithExcess.set(0, firstUser.changeDelta(newDelta));
 
-        // Establish all transactions needed to settle accounts
-        ArrayList<TransactionsDTO> listOfTransactions = new ArrayList<>();
-        while (e >= 0) {
-            UserDOO userWithExcess = usersWithExcess.get(e);
-            UserDOO userWithDeficit = usersWithDeficit.get(d);
+        int e = usersWithExcess.size()-1;
+        int d = usersWithDeficit.size()-1;
+        return computeTransactionsDTOS(usersWithExcess, usersWithDeficit, e, d);
+    }
+
+    private List<TransactionsDTO> computeTransactionsDTOS(List<UserDOO> usersWithExcess, List<UserDOO> usersWithDeficit, int excessIndex, int deficitIndex) {
+        List<TransactionsDTO> listOfTransactions = new ArrayList<>();
+        while (excessIndex >= 0) {
+            UserDOO userWithExcess = usersWithExcess.get(excessIndex);
+            UserDOO userWithDeficit = usersWithDeficit.get(deficitIndex);
             double exc = userWithExcess.userDelta();
             double defi = userWithDeficit.userDelta();
             if (exc == defi) {
-                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(d).userName(), usersWithExcess.get(e).userName(), exc, Currency.EUR));  // The handling of different currencies is here being prepared for, but will only be implemented at a later point in time.
-                e--;
-                d--;
-            }
-            else if (exc > defi) {
-                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(d).userName(), usersWithExcess.get(e).userName(), defi, Currency.EUR));
-                d--;
-                usersWithExcess.set(e, userWithExcess.changeDelta(Math.round((exc-defi)*100)/100.0));
-            }
-            else {
-                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(d).userName(), usersWithExcess.get(e).userName(), exc, Currency.EUR));
-                e--;
-                usersWithDeficit.set(d, userWithDeficit.changeDelta(Math.round((defi-exc)*100)/100.0));
+                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(deficitIndex).userName(), usersWithExcess.get(excessIndex).userName(), exc, Currency.EUR));  // The handling of different currencies is here being prepared for, but will only be implemented at a later point in time.
+                excessIndex--;
+                deficitIndex--;
+            } else if (exc > defi) {
+                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(deficitIndex).userName(), usersWithExcess.get(excessIndex).userName(), defi, Currency.EUR));
+                deficitIndex--;
+                usersWithExcess.set(excessIndex, userWithExcess.changeDelta(Math.round((exc-defi)*100)/100.0));
+            } else {
+                listOfTransactions.add(new TransactionsDTO(usersWithDeficit.get(deficitIndex).userName(), usersWithExcess.get(excessIndex).userName(), exc, Currency.EUR));
+                excessIndex--;
+                usersWithDeficit.set(deficitIndex, userWithDeficit.changeDelta(Math.round((defi-exc)*100)/100.0));
             }
         }
         return listOfTransactions;
+    }
+
+    private double getArithMean(List<User> userList) {
+        int numberOfPersons = userList.size();
+        double arithMean = Math.round(getSum()/numberOfPersons*100)/100.0;
+        if (arithMean==0.0) {
+            throw new RuntimeException("There is no sum big enough to be divided.");
+        }
+        return arithMean;
+    }
+
+    private void determinUserLists(List<User> userList, double arithMean, List<UserDOO> usersWithExcess, List<UserDOO> usersWithDeficit) {
+        for (User value : userList) {
+            String user = value.getUsername();
+            double sum = getSumByUser(user);
+            if ((sum - arithMean) >= 0.0) {
+                double delta = Math.round((sum - arithMean) * 100) / 100.0;
+                var userRecord = new UserDOO(user, delta);
+                usersWithExcess.add(userRecord);
+            } else {
+                double delta = Math.round((arithMean - sum) * 100) / 100.0;
+                var userRecord = new UserDOO(user, delta);
+                usersWithDeficit.add(userRecord);
+            }
+        }
+        usersWithExcess.sort(Comparator.comparing(UserDOO::userDelta).reversed());
+        usersWithDeficit.sort(Comparator.comparing(UserDOO::userDelta).reversed());
     }
 }
--- backend/src/test/java/de/neuefische/smartcount/SmartCountControllerTest.java
+++ backend/src/test/java/de/neuefische/smartcount/SmartCountControllerTest.java
@@ -1,20 +1,45 @@
 package de.neuefische.smartcount;
 
 import de.neuefische.smartcount.Users.UserController;
+import de.neuefische.smartcount.Users.UserCreationData;
+import de.neuefische.smartcount.Users.UserLoginData;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static de.neuefische.smartcount.Currency.EUR;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SmartCountControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
-/*
+
+    @Test
+    void integrationTest() {
+
+        // should register new user
+        UserCreationData createUser1 = new UserCreationData();
+        createUser1.setUsername("Alfred");
+        createUser1.setPassword("234SoSafe");
+        createUser1.setPasswordAgain("234SoSafe");
+
+        ResponseEntity<String> responseRegister = restTemplate.postForEntity("/api/register", createUser1, String.class);
+
+        assertThat(responseRegister.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertEquals("user successfully created", responseRegister.getBody());
+
+        // should log in user
+
+
+}
+
+    /*
     @Test
     void addExpenses() {
 */
